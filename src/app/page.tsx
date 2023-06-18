88e38444a23d2f7e4adae7a1dbdf0016fe115877
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-18 17:02:35 +0700

    Add language switch button
    only design (for now it cannot switch content language)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 // pre-loaded custom fonts
+import SwitchButton from "./components/SwitchButton";
 import { chopsin, anantason, sukhumvit_medium, sukhumvit_text } from "./fonts";
 import "./page.css";
 
 export default function Home() {
-    return (
-        <div className="header">
-            <h1
-                className={`${chopsin.className} text-9xl`}
-                id="lakronvidva-heading"
-            >
-                LAKORNVIDVA
-            </h1>
-        </div>
-    );
+  return (
+    <div className="header">
+      <SwitchButton></SwitchButton>
+      <h1 className={`${chopsin.className} text-9xl`} id="lakronvidva-heading">
+        LAKORNVIDVA
+      </h1>
+    </div>
+  );
 }
