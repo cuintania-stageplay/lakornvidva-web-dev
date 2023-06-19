c67e0ecf0a13fc7fd4983c98e5a09cd88a81affa
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-19 15:36:55 +0700

    Add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 // pre-loaded custom fonts
 import { chopsin, anantason, sukhumvit_medium, sukhumvit_text } from "./fonts";
 import "./page.css";
+import Image from "next/image";
 import { styled } from "styled-components";
+
+import Logo from "./images/logo-white.png";
 
 import SwitchButton from "./components/SwitchButton";
 import Header from "./components/Header";
@@ -14,12 +17,18 @@ import Sponsor from "./components/Sponsor";
 
 const Homepage = styled.html`
   background-image: radial-gradient(
-      rgba(17, 16, 11, 0.5) 5%,
+      rgba(17, 16, 11, 0.75) 5%,
       rgba(17, 16, 11, 1) 50%
     ),
     url(https://grainy-gradients.vercel.app/noise.svg);
-  width: 100%;
-  height: 100%;
+  height: auto;
+`;
+
+const Bar = styled.div`
+  padding: 25px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const Body = styled.div`
@@ -31,7 +40,10 @@ const Body = styled.div`
 export default function Home() {
   return (
     <Homepage>
-      <SwitchButton />
+      <Bar>
+        <SwitchButton />
+        <Image src={Logo.src} width={150} height={150} alt="LAKORNVIDVA" />
+      </Bar>
       <Body>
         <Header />
         <Subtitle />
