7b4ec6690ddb12eb326c30f5cc5cd8f80264e3a3
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-19 14:52:22 +0700

    Add sponsor part

---
+++ src/app/components/Sponsor.tsx
@@ -0,0 +1,37 @@
+import { styled } from "styled-components";
+import { anantason, chopsin, sukhumvit_text } from "../fonts";
+
+const SectionLine = styled.div`
+  width: 1029px;
+  height: 2px;
+
+  background: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0.131823) 0%,
+    #ffffff 42.14%,
+    #ffffff 56.21%,
+    rgba(255, 255, 255, 0) 100%
+  );
+`;
+
+const SponsorName = styled.div`
+  width: 1029px;
+
+  padding-top: 20px;
+  font-family: "Cordia New";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+export default function Sponsor() {
+  return (
+    <div>
+      <SectionLine />
+      <SponsorName>Sponsor1 Sponsor2 Sponsor3</SponsorName>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import Synopsis from "./components/Synopsis";
 import Subtitle from "./components/Subtitle";
 import BuyButton from "./components/BuyButton";
 import SocialMedia from "./components/SocialMedia";
+import Sponsor from "./components/Sponsor";
 
 const Homepage = styled.html`
   background-image: radial-gradient(
@@ -37,6 +38,7 @@ export default function Home() {
         <Synopsis />
         <BuyButton />
         <SocialMedia />
+        <Sponsor />
       </Body>
     </Homepage>
   );
