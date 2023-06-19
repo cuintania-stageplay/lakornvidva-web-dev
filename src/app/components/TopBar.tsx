3bb7a70fc74e258a09289896f1899e52e1d826c8
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-06-19 22:24:40 +0700

    fix bugs double html tags. will result in ssr

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -5,17 +5,17 @@ import Logo from "../images/logo-white.png";
 import SwitchButton from "./SwitchButton";
 
 const Bar = styled.div`
-  padding: 25px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+    padding: 25px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `;
 
 export default function TopBar() {
-  return (
-    <Bar>
-      <SwitchButton />
-      <Image src={Logo.src} width={150} height={150} alt="LAKORNVIDVA" />
-    </Bar>
-  );
+    return (
+        <Bar>
+            <SwitchButton />
+            <Image src={Logo.src} width={150} height={150} alt="LAKORNVIDVA" />
+        </Bar>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,11 @@ import { styled } from "styled-components";
 import Body from "./components/Body";
 import TopBar from "./components/TopBar";
 
-const Homepage = styled.html`
-  background-image: radial-gradient(
-      rgba(17, 16, 11, 0.75) 5%,
-      rgba(17, 16, 11, 1) 50%
-    ),
-    url(https://grainy-gradients.vercel.app/noise.svg);
-  height: auto;
-`;
-
 export default function Home() {
-  return (
-    <Homepage>
-      <TopBar />
-      <Body />
-    </Homepage>
-  );
+    return (
+        <>
+            <TopBar />
+            <Body />
+        </>
+    );
 }
