fc61a7070bfb414e5ce2f1a21e37f6a3683d8030
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-20 13:28:29 +0700

    Add hover icon style

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -9,20 +9,39 @@ const IconBox = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+`;
+
+const IconStyle = styled.a`
+  border-width: 6px;
+  border-radius: 50%;
+
+  &:hover {
+    border-color: ${(props) => props.color};
+  }
 `;
 
 export default function SocialMedia() {
   return (
     <IconBox>
-      <a href="https://www.facebook.com/culakornvidva/?locale=hi_IN">
+      <IconStyle
+        color="#00E225"
+        href="https://www.facebook.com/culakornvidva/?locale=hi_IN"
+      >
         <Image src={facebookIcon.src} width={100} height={100} alt="FB" />
-      </a>
-      <a href="https://www.instagram.com/lakornvidva/?fbclid=IwAR0vLYfwf-zvx3zeYrm2imllb3WnkPMNTx9T7J4p6pI1NTkA3TipODF_jYk">
+      </IconStyle>
+      <IconStyle
+        color="#358FAF"
+        href="https://www.instagram.com/lakornvidva/?fbclid=IwAR0vLYfwf-zvx3zeYrm2imllb3WnkPMNTx9T7J4p6pI1NTkA3TipODF_jYk"
+      >
         <Image src={igIcon.src} width={100} height={100} alt="IG" />
-      </a>
-      <a href="https://www.facebook.com/culakornvidva/?locale=hi_IN">
+      </IconStyle>
+      <IconStyle
+        color="#E93224"
+        href="https://www.facebook.com/culakornvidva/?locale=hi_IN"
+      >
         <Image src={tiktokIcon.src} width={100} height={100} alt="Tiktok" />
-      </a>
+      </IconStyle>
     </IconBox>
   );
 }
