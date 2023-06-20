ec68fd989d2ed2d03a9898e4bcbc3d126fe385cf
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-20 20:36:00 +0700

    Add Background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,22 @@ import { styled } from "styled-components";
 import Body from "./components/Body";
 import TopBar from "./components/TopBar";
 
+import Noise from "./images/noise.png";
+import Partical from "./images/particle.png";
+import Gradient from "./images/gradient.png";
+
 const Homepage = styled.div`
-  background-image: radial-gradient(
-      rgba(17, 16, 11, 0.75) 5%,
-      rgba(17, 16, 11, 1) 50%
-    ),
-    url(https://grainy-gradients.vercel.app/noise.svg);
-  height: auto;
+  background-image: url(${Noise.src}), url(${Gradient.src}),
+    url(${Partical.src});
+  background-color: #11100b;
+  mix-blend-mode: normal, luminosity, darken;
+  background-size: contain, cover, 50%;
+
+  background-position: top, bottom, bottom;
+
+  background-blend-mode: normal, overlay, normal;
+  background-repeat: no-repeat;
+  backdrop-filter: brightness(60%), normal, overlay;
 `;
 
 export default function Home() {
