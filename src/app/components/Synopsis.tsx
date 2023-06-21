261ab56a9bb38fa97836fdffbb8b353e8c8a82b3
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-21 15:09:06 +0700

    Add see more feature

--- src/app/components/Synopsis.tsx
+++ src/app/components/Synopsis.tsx
@@ -1,6 +1,7 @@
 import { styled } from "styled-components";
 import { sukhumvit_medium } from "../fonts";
 import { synopsis } from "../text/Text";
+import { useState } from "react";
 
 const SynopsisStyle = styled.div`
   font-family: ${sukhumvit_medium.style.fontFamily};
@@ -8,7 +9,10 @@ const SynopsisStyle = styled.div`
   font-weight: 400;
   font-size: 40px;
 
+  width: 1400px;
+
   padding: 160px;
+  padding-bottom: 20px;
 
   text-align: center;
 
@@ -19,9 +23,40 @@ const SynopsisStyle = styled.div`
   text-fill-color: transparent;
 
   -webkit-text-stroke-color: #000000;
-  -webkit-text-stroke-width: 0.5px;
+  -webkit-text-stroke-width: 0.75px;
+`;
+
+const Seemore = styled.button`
+  font-family: ${sukhumvit_medium.style.fontFamily};
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  color: rgba(255, 255, 255, 0.25);
+
+  text-align: right;
+
+  width: 1400px;
+
+  text-decoration-line: underline;
+  text-decoration-color: rgba(255, 255, 255, 0.25);
 `;
 
 export default function Synopsis(state: { language: string }) {
-  return <SynopsisStyle>{synopsis.get(state.language)}</SynopsisStyle>;
+  const [isSeemore, setIsSeemore] = useState(true);
+  return (
+    <>
+      <SynopsisStyle>
+        {isSeemore
+          ? synopsis.get(state.language)?.substring(0, 194)
+          : synopsis.get(state.language)}
+      </SynopsisStyle>
+      <Seemore
+        onClick={() => {
+          setIsSeemore(!isSeemore);
+        }}
+      >
+        {isSeemore ? "See more" : "Show less"}
+      </Seemore>
+    </>
+  );
 }
