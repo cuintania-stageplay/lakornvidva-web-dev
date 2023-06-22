2d78aa8b8b193729fa8c1d236e369ad325fbdfea
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-22 13:40:10 +0700

    Add sponsor

--- src/app/components/Sponsor.tsx
+++ src/app/components/Sponsor.tsx
@@ -1,11 +1,14 @@
+import Image from "next/image";
 import { styled } from "styled-components";
 import { anantason, chopsin, sukhumvit_text } from "../fonts";
+import SponsorPic01 from "../images/GISC_Logo.png";
+import SponsorPic02 from "../images/CPALL_Logo.png";
 
 const SectionLine = styled.div`
-  width: 1029px;
+  width: 53.5vw;
   height: 2px;
 
-  margin-top: 50px;
+  margin-top: 5.5vh;
 
   background: linear-gradient(
     90deg,
@@ -17,24 +20,66 @@ const SectionLine = styled.div`
 `;
 
 const SponsorName = styled.div`
-  width: 1029px;
-
-  padding-top: 20px;
-  padding-bottom: 100px;
+  width: 53.5vw;
+  padding-top: 2vw;
+  padding-bottom: 8vw;
   font-family: "Cordia New";
   font-style: normal;
   font-weight: 400;
   font-size: 36px;
-  text-align: center;
 
   color: #ffffff;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const Wrapper = styled.div`
+  width: max(50px, 6.5vw);
+  background: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0.1) 0%,
+    #ffffff 42.14%,
+    #ffffff 56.21%,
+    rgba(255, 255, 255, 0.1) 100%
+  );
+`;
+
+const SponsorStyle = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: relative;
+  bottom: 0px;
 `;
 
 export default function Sponsor() {
   return (
-    <div>
+    <SponsorStyle>
       <SectionLine />
-      <SponsorName>Sponsor1 Sponsor2 Sponsor3</SponsorName>
-    </div>
+      <SponsorName>
+        <Wrapper>
+          <Image
+            layout="responsive"
+            src={SponsorPic01.src}
+            width={517}
+            height={339}
+            alt="gis"
+          />
+        </Wrapper>
+        <Wrapper>
+          <Image
+            layout="responsive"
+            src={SponsorPic02.src}
+            width={7993}
+            height={5188}
+            alt="CPALL"
+          />
+        </Wrapper>
+      </SponsorName>
+    </SponsorStyle>
   );
 }
