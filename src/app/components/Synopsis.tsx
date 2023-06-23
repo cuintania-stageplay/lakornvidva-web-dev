a965cb3e2d71162fb8313c258f63f8ae7c2ccb2b
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-06-23 13:08:15 +0700

    Adjust synopsis into paragraph

--- src/app/components/Synopsis.tsx
+++ src/app/components/Synopsis.tsx
@@ -3,15 +3,15 @@ import { sukhumvit_medium } from "../fonts";
 import { synopsis } from "../text/Text";
 import { useState } from "react";
 
-const SynopsisStyle = styled.div<{ isSeeMore?: boolean }>`
+const SynopsisStyle = styled.pre<{ isSeeMore?: boolean }>`
   font-family: ${sukhumvit_medium.style.fontFamily};
   font-style: normal;
   font-weight: 400;
-  font-size: max(12px, 2.23vw);
+  font-size: max(12px, 1.8vw);
 
   width: 73vw;
 
-  line-height: 3;
+  line-height: 2;
   overflow: hidden;
   display: -webkit-box;
   -webkit-line-clamp: ${(props) => (props.isSeeMore ? "3" : "none")};
@@ -35,11 +35,23 @@ const SynopsisStyle = styled.div<{ isSeeMore?: boolean }>`
   -webkit-text-stroke-width: 0.04vw;
 `;
 
+const SynopsisBig = styled(SynopsisStyle)`
+  @media screen and (max-width: 610px) {
+    display: none;
+  }
+`;
+
+const SynopsisSmall = styled(SynopsisStyle)`
+  @media screen and (min-width: 609px) {
+    display: none;
+  }
+`;
+
 const SynopsisBox = styled.div`
   /*min-height: 50vw;*/
   display: flex;
   justify-content: flex-start;
-  align-items: center;
+  align-items: flex-end;
   flex-direction: column;
 
   padding-bottom: max(25px, 15vw);
@@ -49,12 +61,10 @@ const Seemore = styled.button`
   font-family: ${sukhumvit_medium.style.fontFamily};
   font-style: normal;
   font-weight: 400;
-  font-size: max(12px, 2.23vw);
+  font-size: max(12px, 1.8vw);
   color: rgba(255, 255, 255, 0.5);
 
   text-align: right;
-
-  width: 73vw;
 
   text-decoration-line: underline;
   text-decoration-color: rgba(255, 255, 255, 0.25);
@@ -64,9 +74,12 @@ export default function Synopsis(state: { language: string }) {
   const [isSeemore, setIsSeemore] = useState(true);
   return (
     <SynopsisBox>
-      <SynopsisStyle isSeeMore={isSeemore}>
-        {synopsis.get(state.language)}
-      </SynopsisStyle>
+      <SynopsisBig isSeeMore={isSeemore}>
+        {synopsis.get(state.language + "_Big")}
+      </SynopsisBig>
+      <SynopsisSmall isSeeMore={isSeemore}>
+        {synopsis.get(state.language + "_Small")}
+      </SynopsisSmall>
       <Seemore
         onClick={() => {
           setIsSeemore(!isSeemore);
