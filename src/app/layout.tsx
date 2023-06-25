1f54bf785e28da3ceee25675856820168ca5c4a9
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-06-25 09:56:30 +0700

    feat: :sparkles: server side style sheer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import StyledComponentsRegistry from './lib/registry'
 import "./globals.css";
 
 export const metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+            <StyledComponentsRegistry>{children}</StyledComponentsRegistry>    
+            </body>
         </html>
     );
 }
