738eb69ea6466fbbcad48ed4c2f13c4c5cf88c53
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-06-25 11:11:38 +0700

    style: :art: format layout.tsx with prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import StyledComponentsRegistry from './lib/registry'
-import "./globals.css";
+import StyledComponentsRegistry from './lib/registry';
+import './globals.css';
 
 export const metadata = {
-    title: "LakornVidva - ละครวิดวะ",
-    description:
-        "ละครเวทีของนิสิต วิศวฯ จุฬาฯ ที่จะพาคุณมาร่วมค้นหา 'หน้าตาความสำเร็จ' ของตัวเอง ผ่านเรื่องราวของหนทางไปสู่ประตูนิรันดร์ ประตูแห่งความสำเร็จในชีวิต",
+  title: 'LakornVidva - ละครวิดวะ',
+  description:
+    "ละครเวทีของนิสิต วิศวฯ จุฬาฯ ที่จะพาคุณมาร่วมค้นหา 'หน้าตาความสำเร็จ' ของตัวเอง ผ่านเรื่องราวของหนทางไปสู่ประตูนิรันดร์ ประตูแห่งความสำเร็จในชีวิต",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body>
-            <StyledComponentsRegistry>{children}</StyledComponentsRegistry>    
-            </body>
-        </html>
-    );
+  return (
+    <html lang='en'>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
