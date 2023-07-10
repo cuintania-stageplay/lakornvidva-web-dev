ab167fdd666651b0d422cb676b24251217e5da8a
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-07-10 15:47:53 +0700

    Add all character to character pane

--- src/app/components/CharacterList.tsx
+++ src/app/components/CharacterList.tsx
@@ -1,16 +1,96 @@
+import { styled } from 'styled-components';
 import CharacterIcon from './CharacterIcon';
 import { RyuIcon, RyuFront, RyuBack } from './CharacterPicture';
+import { PluIcon, PluFront, PluBack } from './CharacterPicture';
+import { NampudIcon, NampudFront, NampudBack } from './CharacterPicture';
+import { PatIcon, PatFront, PatBack } from './CharacterPicture';
+import { FriendIcon, FriendFront, FriendBack } from './CharacterPicture';
+import { AomIcon, AomFront, AomBack } from './CharacterPicture';
+import { AutoIcon, AutoFront, AutoBack } from './CharacterPicture';
+import { DenaIcon, DenaFront, DenaBack } from './CharacterPicture';
+import { BoomIcon, BoomFront, BoomBack } from './CharacterPicture';
+import { MagicIcon, MagicFront, MagicBack } from './CharacterPicture';
+
+const CharacterBox = styled.div`
+  width: 73vw;
+
+  padding: 5vw;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
 
 export default function CharacterList() {
   return (
     <>
-      {RyuIcon.src}
-      <CharacterIcon
-        characterName='Ryu'
-        icon={RyuIcon.src}
-        frontCard={RyuFront.src}
-        backCard={RyuBack.src}
-      />
+      <CharacterBox>
+        <CharacterIcon
+          characterName='Ryu'
+          icon={RyuIcon.src}
+          frontCard={RyuFront.src}
+          backCard={RyuBack.src}
+        />
+        <CharacterIcon
+          characterName='Plu'
+          icon={PluIcon.src}
+          frontCard={PluFront.src}
+          backCard={PluBack.src}
+        />
+        <CharacterIcon
+          characterName='Nampud'
+          icon={NampudIcon.src}
+          frontCard={NampudFront.src}
+          backCard={NampudBack.src}
+        />
+        <CharacterIcon
+          characterName='Pat'
+          icon={PatIcon.src}
+          frontCard={PatFront.src}
+          backCard={PatBack.src}
+        />
+      </CharacterBox>
+
+      <CharacterBox>
+        <CharacterIcon
+          characterName='Friend'
+          icon={FriendIcon.src}
+          frontCard={FriendFront.src}
+          backCard={FriendBack.src}
+        />
+        <CharacterIcon
+          characterName='Aom'
+          icon={AomIcon.src}
+          frontCard={AomFront.src}
+          backCard={AomBack.src}
+        />
+        <CharacterIcon
+          characterName='Auto'
+          icon={AutoIcon.src}
+          frontCard={AutoFront.src}
+          backCard={AutoBack.src}
+        />
+        <CharacterIcon
+          characterName='Dena'
+          icon={DenaIcon.src}
+          frontCard={DenaFront.src}
+          backCard={DenaBack.src}
+        />
+      </CharacterBox>
+      <CharacterBox>
+        <CharacterIcon
+          characterName='Boom'
+          icon={BoomIcon.src}
+          frontCard={BoomFront.src}
+          backCard={BoomBack.src}
+        />
+        <CharacterIcon
+          characterName='Magic'
+          icon={MagicIcon.src}
+          frontCard={MagicFront.src}
+          backCard={MagicBack.src}
+        />
+      </CharacterBox>
     </>
   );
 }
