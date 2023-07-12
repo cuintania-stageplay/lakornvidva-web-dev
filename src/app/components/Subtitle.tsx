12b9e34e44c164c5b148c574ec0091988d4ea1ee
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2023-07-12 13:02:35 +0700

    Change subtitle text style

--- src/app/components/Subtitle.tsx
+++ src/app/components/Subtitle.tsx
@@ -5,27 +5,44 @@ import { quote } from '../text/Text';
 const Quote = styled.div`
   font-family: ${sukhumvit_medium.style.fontFamily};
   font-size: max(12px, 2.23vw);
-  font-weight: 1200;
+  font-weight: 900;
   text-align: center;
 
-  padding-top: 1.38vh;
+  position: absolute;
 
   background: radial-gradient(
     50% 50% at 50% 50%,
     #ffffff 0%,
-    rgba(255, 255, 255, 0.51) 100%
+    rgba(150, 150, 150, 0.75) 100%
   );
+
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   background-clip: text;
   text-fill-color: transparent;
+`;
 
-  -webkit-text-stroke-color: black;
-  -webkit-text-stroke-width: 0.02vw;
+const QuoteBorder = styled(Quote)`
+  text-shadow: -0.05vw -0.05vw 0 #006a04, 0 -0.05vw 0 #006a04,
+    0.05vw -0.05vw 0 #006a04, 0.05vw 0 0 #006a04, 0.05vw 0.05vw 0 #006a04,
+    0 0.05vw 0 #006a04, -0.05vw 0.05vw 0 #006a04, -0.05vw 0 0 #006a04;
 
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  -webkit-filter: drop-shadow(-4px -4px 10px rgba(228, 68, 68, 0.8));
+`;
+
+const QuoteBox = styled.div`
+  width: 100vw;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default function Subtitle() {
-  return <Quote>{quote}</Quote>;
+  return (
+    <QuoteBox>
+      <QuoteBorder>{quote}</QuoteBorder>
+      <Quote>{quote}</Quote>
+    </QuoteBox>
+  );
 }
